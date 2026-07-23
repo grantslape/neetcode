44bd910a7dd153bed627fd56ef946abc84ba6a7c
Grant Slape <me@grantslape.com>
2026-07-22 20:14:46 -0500

    cleanup: remove dead, old main.go, unneeded with current runnable file paradigm

--- main.go
+++
@@ -1,6 +0,0 @@
-package main
-
-// This is where I usually put the problem I'm currently working on.
-// You can find the problems I remembered to archive in the completed directory
-
-func main() {}
